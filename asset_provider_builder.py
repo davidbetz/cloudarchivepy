6a4dfd04339004c6fb0e715faf23f6c619497b7f
David Betz <dfb@davidbetz.net>
2016-07-29 15:03:49 -0500

    enable s3

--- asset_provider_builder.py
+++ asset_provider_builder.py
@@ -1,7 +1,7 @@
 import config
 
 from storage_azure.provider import AzureAssetProvider
-#from storage_s3.provider import S3AssetProvider
+from storage_s3.provider import S3AssetProvider
 
 def create(area_config):
     storage_name = area_config['storage']
@@ -11,8 +11,7 @@ def create(area_config):
     if name == "azure":
         provider = AzureAssetProvider()
     elif name == "s3":
-        pass
-        #provider = S3AssetProvider()
+        provider = S3AssetProvider()
     else:
         raise ValueError("no")
 
